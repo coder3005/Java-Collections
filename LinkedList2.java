d8a71d8542a9c6e6ce6963badaf71482ca765a8b
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-29 11:28:00 +0530

    Create LinkedList2.java

---
+++ LinkedList2.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+class javaCollections {
+
+    public static void main(String[] args) {
+        
+        LinkedList<String> Name=new LinkedList<String>();
+        Name.add("Ankit");
+        Name.add("Ankush");
+        Name.add("Abhi");
+
+        System.out.println(Name);
+        Name.removeFirst();
+        System.out.print(Name);
+        
+    }
+}
