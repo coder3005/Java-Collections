6a5140406a39cf89cbff112fc50d7061616c9505
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-29 11:18:18 +0530

    Create Collections2.java

---
+++ Collections2.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class javaCollections {
+
+    public static void main(String[] args) {
+        
+        ArrayList<String> Name=new ArrayList<String>();
+        Name.add("Ankit");
+        Name.add("Ankush");
+        Name.add("Abhi");
+
+        System.out.println(Name);
+        Name.set(0,"Rohit");
+        System.out.println(Name);
+    }
+}
